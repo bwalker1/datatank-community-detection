f507a183b58e4a9ecd6542859a65e17f40b8973d
bwalker1 <bwalker@unc.edu>
2019-09-16 14:57:13 -0400

    cleaned up script a bunch and changed to single file input

--- analysis.tank/Attachments/extract_time_info/extract_time_info/main.cpp
+++ analysis.tank/Attachments/extract_time_info/extract_time_info/main.cpp
@@ -17,45 +17,64 @@
 
 #include <math.h>
 
-DTDoubleArray Computation(const DTSeriesPath3D &series);
+
+// Include DTSource.h if you want to include all the headers.
+
+#include "DTArguments.h"
+#include "DTSaveError.h"
+
+#include "DTDataFile.h"
+#include "DTDoubleArray.h"
+#include "DTSeriesRealNumber.h"
+
+// Common utilities
+#include "DTDoubleArrayOperators.h"
+#include "DTProgress.h"
+#include "DTTimer.h"
+#include "DTUtilities.h"
+#include "DTDictionary.h"
+
+#include <math.h>
+
+DTDoubleArray Computation(const DTSeriesRealNumber &series);
 
 int main(int argc,const char *argv[])
 {
     DTSetArguments(argc,argv);
-
+    
     DTDataFile inputFile("Input.dtbin",DTFile::ReadOnly);
     // Read in the input variables.
-    DTSeriesPath3D series;
+    DTSeriesRealNumber series;
     Read(inputFile,"series",series);
-
+    
     // The computation.
     DTDoubleArray computed;
     clock_t t_before = clock();
     computed = Computation(series);
     clock_t t_after = clock();
     double exec_time = double(t_after-t_before)/double(CLOCKS_PER_SEC);
-
+    
     // Write the output.
     DTDataFile outputFile("Output.dtbin",DTFile::NewReadWrite);
-
+    
     // Output from computation
     outputFile.Save(computed,"Var");
     outputFile.Save("NumberList","Seq_Var");
-
+    
     // The execution time.
     outputFile.Save(exec_time,"ExecutionTime");
     outputFile.Save("Real Number","Seq_ExecutionTime");
-
+    
     // The errors.
     DTSaveError(outputFile,"ExecutionErrors");
     outputFile.Save("StringList","Seq_ExecutionErrors");
-
+    
     outputFile.SaveIndex();
-
+    
     return 0;
 }
 
-DTDoubleArray Computation(const DTSeriesPath3D &series)
+DTDoubleArray Computation(const DTSeriesRealNumber &series)
 {
     DTMutableDoubleArray toReturn(2);
     
